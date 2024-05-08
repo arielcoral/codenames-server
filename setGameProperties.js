845661256b93e68c92fbc267328c84fa811c4e59
Ariel Coral <arielcoral180@gmail.com>
2024-05-08 19:50:25 +0300

    deleted one file

--- setGameProperties.js
+++
@@ -1,8 +0,0 @@
-const setGameProperties = (data, gameProperties) => {
-    let updatedGameProperties = gameProperties;
-    for (const [key, value] of Object.entries(data)) {
-        updatedGameProperties[key] = value;
-    }
-    return updatedGameProperties
-}
-module.exports = setGameProperties;
