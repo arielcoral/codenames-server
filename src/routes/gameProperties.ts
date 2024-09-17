02e25fe76cf0faf3a31c952c72d4998ed52919af
origiladi <120328585+origiladi@users.noreply.github.com>
2024-09-17 09:52:43 +0300

    commented setGamePropertiesSchema because it throw errors all the time

--- src/routes/gameProperties.ts
+++ src/routes/gameProperties.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createBoard, deleteGameAfterFinishing, getGamePropertiesByChatRoom, setGameProperties } from "../controllers/gameProperties.controllers";
 import { celebrate } from "celebrate";
-import { createGamePropertiesSchema, setGamePropertiesSchema } from "../middlewares/gameProperties.schema";
+import { createGamePropertiesSchema, /*setGamePropertiesSchema*/ } from "../middlewares/gameProperties.schema";
 import { noBodySchema } from "../middlewares/user.schema";
 
 const gamePropertiesRouter: Router = Router();
@@ -9,6 +9,6 @@ const gamePropertiesRouter: Router = Router();
 gamePropertiesRouter.post('/', celebrate(createGamePropertiesSchema), createBoard)
 gamePropertiesRouter.get('/:chatRoom', celebrate(noBodySchema), getGamePropertiesByChatRoom)
 gamePropertiesRouter.delete('/:chatRoom', celebrate(noBodySchema), deleteGameAfterFinishing)
-gamePropertiesRouter.patch('/', celebrate(setGamePropertiesSchema), setGameProperties)
+gamePropertiesRouter.patch('/',/* celebrate(setGamePropertiesSchema),*/ setGameProperties)
 
 export default gamePropertiesRouter; 
